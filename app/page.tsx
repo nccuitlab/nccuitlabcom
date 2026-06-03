9f5cd778350a7536c8d0188d27ec65794e8109d2
xinai lin <xica@xinais-MacBook-Air-556.local>
2026-06-03 21:02:27 +0800

    Remove landing page white margin

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
-    <main style={{ margin: 0, padding: 0, minHeight: "100vh" }}>
-      <iframe
-        src="/itlab.html"
-        title="ITLab"
-        style={{
-          width: "100%",
-          height: "100vh",
-          border: "none",
-          display: "block",
-        }}
-      />
-    </main>
+    <iframe
+      src="/itlab.html"
+      title="ITLab"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        display: "block",
+        margin: 0,
+        padding: 0,
+      }}
+    />
   );
 }
